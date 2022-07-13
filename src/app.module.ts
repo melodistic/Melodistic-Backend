5b89ead4e48e07d7c0d3c7ebaa9a0035755b6c9c
GGolfz <wisarutgolf11@gmail.com>
2022-07-13 19:37:53 +0700

    add: Track Service :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule
+    AuthModule,
+    TrackModule
   ],
   controllers: [AppController],
   providers: [],
