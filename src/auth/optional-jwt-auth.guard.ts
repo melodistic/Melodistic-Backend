1fe5a8277a464befea1094ff996bdef638e2cc45
GGolfz <wisarutgolf11@gmail.com>
2022-09-14 19:20:00 +0700

    add: is_favorite field when get track :sparkles:

---
+++ src/auth/optional-jwt-auth.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+
+@Injectable()
+export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
+    handleRequest<TUser = any>(err: any, user: any, info: any, context: any, status?: any): TUser {
+        return user;
+    }
+}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { Track, UserFavorite } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
 import { CreateTrackDto } from './dto/create-tack.dto';
 
@@ -10,6 +11,23 @@ export class TrackService {
     private httpService: HttpService,
   ) {}
 
+  preprocessFavoriteTrack(favoriteTrack: any): any {
+    return favoriteTrack.map(
+      (
+        track: Track & {
+          UserFavorite: UserFavorite[];
+        },
+      ) => {
+        const isFav = track.UserFavorite.length > 0;
+        delete track.UserFavorite;
+        return {
+          ...track,
+          is_favorite: isFav,
+        };
+      },
+    );
+  }
+
   async getTrack(): Promise<any> {
     const publicTrack = await this.prisma.track.findMany({
       where: {
@@ -17,6 +35,23 @@ export class TrackService {
       },
     });
     return publicTrack;
+  }
+
+  async getTrackWithFavorite(userId: string): Promise<any> {
+    const favoriteTrack = await this.prisma.track.findMany({
+      where: {
+        is_public: true,
+      },
+      include: {
+        UserFavorite: {
+          where: {
+            user_id: userId,
+          },
+        },
+      },
+    });
+    const modifiedFavoriteTrack = this.preprocessFavoriteTrack(favoriteTrack);
+    return modifiedFavoriteTrack;
   }
 
   async getTrackById(userId: string, trackId: string): Promise<any> {
@@ -46,29 +81,29 @@ export class TrackService {
       {
         program_name: track.program_name,
         muscle_group: track.muscle_group,
-        sections: track.sections
+        sections: track.sections,
       },
     );
     await this.prisma.generatedTrack.create({
       data: {
         user_id: userId,
         track_id: response.data.track_id,
-      }
-    })
+      },
+    });
     return {
       status: 200,
       message: 'Track created successfully',
       track_id: response.data.track_id,
     };
   }
-  
+
   async checkUserTrack(userId: string, trackId: string): Promise<any> {
     const result = await this.prisma.generatedTrack.findFirst({
       where: {
         track_id: trackId,
         user_id: userId,
       },
-    })
+    });
     return result;
   }
 
