fa8352b8ee39227ea3f6c568279a629cfbd8f94c
GGolfz <52349645+GGolfz@users.noreply.github.com>
2022-10-02 19:21:36 +0700

    Update email.ts

--- src/template/email.ts
+++ src/template/email.ts
@@ -29,4 +29,60 @@ export class EmailTemplate {
         </table>
     </body>
     </html>`;
+  
+  renderVerifyEmailTemplate = (email: string, url: string): string =>
+  `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet"
+      />
+    </head>
+    <body>
+      <table
+        style="width: 100%; font-family: 'Poppins', sans-serif; font-size: 14px"
+      >
+        <tr>
+          <td>
+            <div style="max-width: 440px; margin: 0 auto">
+              <div
+                style="font-size: 28px; font-weight: bold; margin-bottom: 16px"
+              >
+                Hi ${email},
+              </div>
+              <div>
+                Thanks for creating a Melodistic Account. Please verify your email
+                address by clicking the button below.
+              </div>
+              <div style="margin: 16px 0">
+                <a href="${url}" style="text-decoration: none">
+                  <div
+                    style="
+                      background: black;
+                      height: 24px;
+                      width: 144px;
+                      margin: 0 auto;
+                      padding: 12px 24px;
+                      color: white;
+                      border-radius: 4px;
+                      cursor: pointer;
+                    "
+                  >
+                    Verify Email Address
+                  </div></a
+                >
+              </div>
+              <div>Thank you for using Melodistic</div>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </body>
+  </html>`;
 }
