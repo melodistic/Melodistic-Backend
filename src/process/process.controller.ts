58a7581fc6eadeb2c429bf00cf1e8968800a3900
GGolfz <wisarutgolf11@gmail.com>
2022-10-12 00:11:36 +0700

    add: get info, process youtube, delete process :sparkles:

--- src/process/process.controller.ts
+++ src/process/process.controller.ts
@@ -1,10 +1,63 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
-import { ProcessService } from "./process.service";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { User } from 'src/decorators/user.decorator';
+import { YoutubeDto } from './dto/youtube.dto';
+import { ProcessService } from './process.service';
 
 @ApiTags('Process')
 @Controller('process')
 export class ProcessController {
   constructor(private processService: ProcessService) {}
-  
+
+  @Get('')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiInternalServerErrorResponse()
+  async getTrack(@User() userId: string): Promise<any> {
+    return this.processService.getProcessInformation(userId);
+  }
+
+  @Post('/youtube')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiInternalServerErrorResponse()
+  async processMusicFromYoutube(
+    @User() userId: string,
+    @Body() data: YoutubeDto,
+  ): Promise<any> {
+    return this.processService.processMusicFromYoutube(userId, data.url);
+  }
+
+  @Post('/file')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiInternalServerErrorResponse()
+  async processMusicFromFile(@User() userId: string): Promise<any> {
+    // Add File Upload here
+    // return this.processService.processMusicFromFile(userId, data.url);
+  }
+
+  @Delete('/:processId')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiInternalServerErrorResponse()
+  async deleteProcessFile(
+    @User() userId: string,
+    @Param('processId') processId: string,
+  ): Promise<any> {
+    return this.processService.deleteProcessFile(userId, processId);
+  }
 }
--- src/process/process.service.ts
+++ src/process/process.service.ts
@@ -1,5 +1,85 @@
-import { Injectable } from "@nestjs/common";
+import { HttpService } from '@nestjs/axios';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ProcessService {
+  constructor(
+    private prisma: PrismaService,
+    private httpService: HttpService,
+  ) {}
+  async getProcessInformation(userId: string): Promise<any> {
+    return this.prisma.processedMusic.findMany({
+      where: {
+        user_id: userId,
+      },
+    });
+  }
+  async processMusicFromYoutube(
+    userId: string,
+    youtubeUrl: string,
+  ): Promise<any> {
+    const video_id = youtubeUrl.includes('v=')
+      ? youtubeUrl.split('v=')[1]
+      : youtubeUrl.split('be/')[1];
+    await this.httpService.axiosRef.post(
+      'https://melodistic.me/api/processor/youtube',
+      {
+        user_id: userId,
+        video_id: video_id,
+      },
+    );
+  }
+  async processMusicFromFile(
+    userId: string,
+    file_name: string,
+    file_path: string,
+  ): Promise<any> {
+    await this.httpService.axiosRef.post(
+      'https://melodistic.me/api/processor/file',
+      {
+        user_id: userId,
+        file_name: file_name,
+        file_path: file_path,
+      },
+    );
+  }
+  async checkProcessFile(userId: string, processId: string): Promise<boolean> {
+    const existProcess = await this.prisma.processedMusic.findFirst({
+      where: {
+        user_id: userId,
+        process_id: processId,
+      },
+    });
+    return existProcess != null;
+  }
+  async deleteProcessFile(userId: string, processId: string): Promise<any> {
+    const isProcessExist = await this.checkProcessFile(userId, processId);
+    if (!isProcessExist) throw new NotFoundException('Process not found');
+
+    const musicIdList = (
+      await this.prisma.processedMusicExtract.findMany({
+        where: {
+          processed_id: processId,
+        },
+      })
+    ).map((music) => music.music_id);
+    await this.prisma.processedMusicExtract.deleteMany({
+      where: {
+        processed_id: processId,
+      },
+    });
+    await this.prisma.music.deleteMany({
+      where: {
+        music_id: {
+          in: musicIdList,
+        },
+      },
+    });
+    return this.prisma.processedMusic.delete({
+      where: {
+        process_id: processId,
+      },
+    });
+  }
 }
