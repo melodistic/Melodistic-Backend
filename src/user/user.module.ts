c3151f150b41f3c9419302bf34677af6d699ce21
GGolfz <wisarutgolf11@gmail.com>
2022-11-04 01:03:51 +0700

    fix: problem :hammer:

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common'
-import { PreprocessorService } from '../utils/preprocessor.service'
-import { PrismaService } from '../prisma.service'
-import { UserController } from './user.controller'
-import { UserService } from './user.service'
-import { TrackService } from 'src/track/track.service'
+import { Module } from '@nestjs/common';
+import { PreprocessorService } from '../utils/preprocessor.service';
+import { PrismaService } from '../prisma.service';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { TrackModule } from 'src/track/track.module';
 
 @Module({
-	providers: [UserService,PrismaService, PreprocessorService],
-	controllers: [UserController],
-	exports: [UserService, TrackService],
+  imports: [TrackModule],
+  providers: [UserService, PrismaService, PreprocessorService],
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
